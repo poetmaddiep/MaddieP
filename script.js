13e4bc24eb9fd2dc9756cf6509e345cdd0d76ca3
poetmaddiep <poet.maddiep@gmail.com>
2025-02-16 22:36:51 -0600

    Update script.js

--- script.js
+++ script.js
@@ -15,15 +15,13 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 });
-function myFunction(imgs) {
-  // Get the expanded image
-  var expandImg = document.getElementById("expandedImg");
-  // Get the image text
-  var imgText = document.getElementById("imgtext");
-  // Use the same src in the expanded image as the image being clicked on from the grid
-  expandImg.src = imgs.src;
-  // Use the value of the alt attribute of the clickable image as text inside the expanded image
-  imgText.innerHTML = imgs.alt;
-  // Show the container element (hidden with CSS)
-  expandImg.parentElement.style.display = "block";
+function openImage(img) {
+    document.getElementById("expandedImg").src = img.src;
+    document.getElementById("imgtext").innerHTML = img.alt;
+    document.getElementById("imageViewer").style.display = "flex";
 }
+
+function closeImage() {
+    document.getElementById("imageViewer").style.display = "none";
+}
+
